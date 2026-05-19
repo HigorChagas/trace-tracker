2de2cf676bb303d7e4594b02c6605797a4d83fef
Higor Chagas <higornchagas@gmail.com>
2026-05-19 18:25:55 -0300

    feat: replace hardcoded error dictionary with database lookup

--- routes/log.py
+++ routes/log.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from fastapi.param_functions import Depends
 
+from database.database import get_async_session
 from schemas.log import Message
 from services.auth import get_current_user
 from services.log import send_log
@@ -9,6 +10,10 @@ router = APIRouter()
 
 
 # TODO: adicionar get_current_user como Depends na rota /search-log/
-@router.post("/search-log/")
-async def search_log(message: Message, current_user=Depends(get_current_user)):
-    return await send_log(message.log)
+@router.post("/search-log/", status_code=status.HTTP_200_OK)
+async def search_log(
+    message: Message,
+    current_user=Depends(get_current_user),
+    session=Depends(get_async_session),
+):
+    return await send_log(session, message.log)
--- services/log.py
+++ services/log.py
@@ -1,29 +1,20 @@
 from fastapi import HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from models.known_error import KnownError
 from services.ai import analyze_error
 
 
-async def send_log(log: str):
-    mapped_errors = {
-        "ZeroDivisionError: division by zero": {
-            "summary": "occurs when you attempt to divide a number by zero or use the modulo operator (%) with a zero divisor.",
-            "severity": "low",
-        },
-        "TypeError": {
-            "summary": "raised when an operation or function is applied to an object of an inappropriate data type",
-            "severity": "low",
-        },
-        "ValueError": {
-            "summary": "occurs when a function or operation receives an argument that is the correct type but has an inappropriate value",
-            "severity": "low",
-        },
-    }
-
-    message_log = log.splitlines()[-1].lower()
-    for error in mapped_errors:
-        error_name = error.lower()
-        if message_log in error_name:
-            return analyze_error(log)
-
-    if not message_log or message_log not in mapped_errors:
+async def send_log(session: AsyncSession, log: str):
+    last_line = log.splitlines()[-1].lower()
+    known_error = await get_known_error(session, last_line)
+    if not known_error:
         raise HTTPException(status_code=404, detail="Error not found")
+    return analyze_error(log)
+
+
+async def get_known_error(session: AsyncSession, log: str):
+    stmt = select(KnownError).where(KnownError.error_name.ilike(f"%{log}%"))
+    result = await session.execute(stmt)
+    return result.scalars().one_or_none()

62a1df34acd6cb1af4274f6d3ec7b86a1659c5ba
Higor Chagas <higornchagas@gmail.com>
2026-05-20 12:05:23 -0300

    feat: add password and email validation on register and login

--- schemas/user.py
+++ schemas/user.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, SecretStr
 
 
 class Register(BaseModel):
-    email: str
-    password: str
+    email: EmailStr = Field(..., min_length=10)
+    password: SecretStr = Field(..., min_length=5, max_length=100)
     name: str
 
 
 class Login(BaseModel):
-    email: str
-    password: str
+    email: EmailStr = Field(..., min_length=10)
+    password: SecretStr = Field(..., min_length=5, max_length=100)
